3f5dae2350cef28fc30a5b2da18993a2218625d2
Ryan Lawrence <ryan.lawrence@gmail.com>
2022-10-31 16:53:29 -0400

    Initial commit: HA monographs list and base styles

---
+++ wp-content/themes/ha-monographs/loop-templates/content-monographs-list.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Partial template for content in page.php
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<section class="monographs-list">
+    <aside class="monograph-card">
+        <div>
+            <h3>Agrimony</h3>
+
+            <p>Agrimony, beggar lice, church steeple, cockleburr, common agrimony,
+                harvest lice, liverwort, sticklewort, stickwort, tea-plant</p>
+        </div>
+    </aside>
+    <aside class="monograph-card">
+        <div>
+            <h3>Agrimony</h3>
+
+            <p>Agrimony, beggar lice, church steeple, cockleburr, common agrimony,
+                harvest lice, liverwort, sticklewort, stickwort, tea-plant</p>
+        </div>
+    </aside>
+    <aside class="monograph-card">
+        <div>
+            <h3>Agrimony</h3>
+
+            <p>Agrimony, beggar lice, church steeple, cockleburr, common agrimony,
+                harvest lice, liverwort, sticklewort, stickwort, tea-plant</p>
+        </div>
+    </aside>
+    <aside class="monograph-card">
+        <div>
+            <h3>Agrimony</h3>
+
+            <p>Agrimony, beggar lice, church steeple, cockleburr, common agrimony,
+                harvest lice, liverwort, sticklewort, stickwort, tea-plant</p>
+        </div>
+    </aside>
+    <aside class="monograph-card">
+        <div>
+            <h3>Agrimony</h3>
+
+            <p>Agrimony, beggar lice, church steeple, cockleburr, common agrimony,
+                harvest lice, liverwort, sticklewort, stickwort, tea-plant</p>
+        </div>
+    </aside>
+    <aside class="monograph-card">
+        <div>
+            <h3>Agrimony</h3>
+
+            <p>Agrimony, beggar lice, church steeple, cockleburr, common agrimony,
+                harvest lice, liverwort, sticklewort, stickwort, tea-plant</p>
+        </div>
+    </aside>
+    <aside class="monograph-card">
+        <div>
+            <h3>Agrimony</h3>
+
+            <p>Agrimony, beggar lice, church steeple, cockleburr, common agrimony,
+                harvest lice, liverwort, sticklewort, stickwort, tea-plant</p>
+        </div>
+    </aside>
+    <aside class="monograph-card">
+        <div>
+            <h3>Agrimony</h3>
+
+            <p>Agrimony, beggar lice, church steeple, cockleburr, common agrimony,
+                harvest lice, liverwort, sticklewort, stickwort, tea-plant</p>
+        </div>
+    </aside>
+</section>
---
+++ wp-content/themes/ha-monographs/page-monographs-list.php
@@ -0,0 +1,47 @@
+<?php
+/* Template Name: Monographs List */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+$container = get_theme_mod( 'understrap_container_type' );
+
+?>
+
+<div class="wrapper" id="page-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<!-- Do the left sidebar check -->
+			<?php //get_template_part( 'global-templates/left-sidebar-check' ); ?>
+
+			<main class="site-main" id="main">
+
+				<?php
+				while ( have_posts() ) {
+					the_post();
+                    // Intro text, pulled from main content editor
+					get_template_part( 'loop-templates/content', 'page' );
+
+                    // List monographs alphabetically (TODO: add pagination)
+					get_template_part( 'loop-templates/content', 'monographs-list' );
+				}
+				?>
+
+			</main><!-- #main -->
+
+			<!-- Do the right sidebar check -->
+			<?php //get_template_part( 'global-templates/right-sidebar-check' ); ?>
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #page-wrapper -->
+
+<?php
+get_footer();
